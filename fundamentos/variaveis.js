9e7961d9fb6589be07a2144015cc4e4571eeaa7c
Joao Fonseca <jf-fonsec@hotmail.com>
2024-06-24 19:53:37 -0300

    descrição das alteraçõe

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,29 @@
+/**
+ * Estudo das Variáveis
+ * @author João Fonseca 
+ */
+
+console.clear()
+console.log("constante______________________________")
+const nome = "João Fonseca"
+console.log(typeof(nome))
+console.log(nome)
+//nome = "João Fonseca"
+console.log("let_____________________________________")
+let linguagem = "Java"
+console.log(linguagem)
+linguagem = "JavaScript"
+console.log(linguagem)
+//let linguagem = "Pyton"
+//console.log(linguagem)
+console.log("var_____________________________________")
+var time = "São Paulo"
+console.log(time)
+time = "Palmeiras"
+console.log(time)
+var time = "Corinthian" // Atenção! Bug's no uso do var
+console.log(time)
+
+
+
+
